4a3de7cd0ad074d96e14df7c55d487be59272187
shawnya <shawnaya@workspace.com>
2023-01-31 20:03:07 -0600

    fizz buzz return program

--- fizzbuzz/fizzbuzz_return.cc
+++ fizzbuzz/fizzbuzz_return.cc
@@ -1,20 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-////////////////////////////////
-// ADD FIZZBUZZ FUNCTION HERE //
-////////////////////////////////
-
-int main ()
+void fizzBuzz (int n)
 {
-  for (int n=1; n<=50; ++n)
-  {
-    ////////////////////////////////////////
-    // ADD CODE TO CALL FIZZBUZZ FUNCTION //
-    // AND PRINT THE RETURNED VALUE HERE  //
-    ////////////////////////////////////////
+  cout << "Please enter a number that will be divisible by 3, 5, or both: "; 
+  cin >> n;
 
-  }
+  if ((n%3==0) && (n%5==0)){
+    cout << "FizzBuzz.";
+  } else if ((n%3)==0){ //if number is divisible by 3
+    cout << "Fizz."; 
+  } else if ((n%5)==0){ // if number is divisible by 5
+    cout<<, "Buzz.";
+  } else 
+    cout << "N."; //if the number is no divisible by either
 }
 
+int main()
+{
+  int n;
+  fizzBuzz(n);
+  return 0;
+}
