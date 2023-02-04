d6e0921fc4dfda9397fb019c348efe7a3352571b
shawnya <shawnaya@workspace.com>
2023-02-03 20:27:44 -0600

    added interative implementation

--- fib_fact/time_fib_iter.cc
+++ fib_fact/time_fib_iter.cc
@@ -7,9 +7,10 @@ using namespace std;
 
 int fib(int N)
 {
-  //////////////////////////////////////////
-  // INSERT ITERATIVE IMPLEMENTATION HERE //
-  //////////////////////////////////////////
+int f[N+1];
+    f[1] = f[2] = 1;
+     for (int i = 3; i <= N; i++)         f[i] = f[i-1] + f[i-2];
+    return f[N];
 }
 
 int main ()
